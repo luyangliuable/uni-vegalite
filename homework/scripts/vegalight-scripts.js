f627c104e06604f03569ebeb420391a2a8ce726e
Lucas Liu <lucas.liu@wexinc.com>
2023-09-26 08:52:26 +1000

    Made chloropleth work.

--- homework/scripts/vegalight-scripts.js
+++ homework/scripts/vegalight-scripts.js
@@ -1,75 +1,51 @@
 // https://mapshaper.org/
 
-// var vlSpec = {
-//     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-//     data: {
-//         values: avgPriceDataset
-//     },
-//     mark: 'point',
-//     encoding: {
-//         y: {
-//             field: 'Suburb',
-//             type: 'nominal'
-//         },
-//         x: {
-//             // aggregate: 'average',
-//             field: 'Price',
-//             type: 'quantitative',
-//             axis: {
-//                 title: 'Average Price'
-//             }
-//         }
-//     }
-// };
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    var topology = 
-        {
-            "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-            "width": 800,
-            "height": 450,
-            "title": "Earthquakes of magnitude 4 or large between 4 Oct 2020 and 10 Oct 2020",
-            // "projection": {"type": "equalEarth"},
-            "layer": [
-                {
-                    "data": {
-                        values: vicSuburbs,
-                        "format": {"property": "features"}
-                    },
-                    "mark": {
-                        "type": "geoshape",
-                        "fill": "lightgray",
-                        "stroke": "white"
-                    }
+    var topology = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+        "width": 800,
+        "height": 450,
+        "title": "Average House Prices by Suburb",
+        "data": {
+            "name": "suburbs",
+            "values": vicSuburbs,
+            "format": {"property": "features"}
+        },
+        "transform": [{
+            "lookup": "properties.Suburb",
+            "from": {
+                "data": {"values": avgPriceDataset},
+                "key": "Suburb",
+                "fields": ["AvgPrice"]
+            }
+        }],
+        "layer": [
+            {
+                "mark": {
+                    "type": "geoshape",
+                    "stroke": "black",
+                    "strokeWidth": 1
                 },
-            ],
-            "transform": [{
-                "lookup": "properties.Suburb",
-                "from": {
-                    "data": {
-                        "values": avgPriceDataset
-                    },
-                    "key": "Suburb",
-                    "fields": ["Suburb"],
-                    "values": ["AvgPrice"]
+                "encoding": {
+                    "color": {"value": "lightgray"}
                 }
-            }],
-            "mark": "geoshape",
-            "encoding": {
-                "tooltip": [
-                    {
-                        "field": "properties.Suburb",
-                        "type": "nominal",
-                        "title": "Suburb"
-                    }
-                ],
-                "color": {
-                    "field": "AvgPrice",
-                    "type": "quantitative",
+            },
+            {
+                "mark": "geoshape",
+                "encoding": {
+                    "color": {
+                        "field": "AvgPrice",
+                        "type": "quantitative",
+                        "legend": {"title": "Average Price"}
+                    },
+                    "tooltip": [
+                        {"field": "properties.Suburb", "type": "nominal", "title": "Suburb"},
+                        {"field": "AvgPrice", "type": "quantitative", "title": "Average Price"}
+                    ]
                 }
             }
-        };
-    // vegaEmbed('#vis', vlSpec);
+        ]
+    };
+
     vegaEmbed('#vis-topology', topology);
 });
